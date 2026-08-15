ed06040c07e8675c93119d6d7bf8b65f532e5bc0
riitae <rita.emiri3@student.uni-pr.edu>
2026-08-15 14:37:33 +0200

    Add continuous sensor monitoring task

--- main/esp32-environment-monitor.c
+++ main/esp32-environment-monitor.c
@@ -1,10 +1,34 @@
 #include <stdio.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
 #include "environmental_sensor.h"
 
-void app_main(void)
+static void sensor_monitor_task(void *pvParameters)
 {
     environmental_data_t data;
 
+    while (1)
+    {
+        if (environmental_sensor_read(&data) == 0)
+        {
+            printf("Temperature: %.1f C\n", data.temperature);
+            printf("Humidity: %.1f %%\n", data.humidity);
+        }
+        else
+        {
+            printf("Failed to read environmental data\n");
+        }
+
+        printf("-----------------------------\n");
+
+        vTaskDelay(pdMS_TO_TICKS(2000));
+    }
+}
+
+void app_main(void)
+{
     if (environmental_sensor_init() != 0)
     {
         printf("Sensor initialization failed\n");
@@ -13,12 +37,12 @@ void app_main(void)
 
     printf("Environmental sensor initialized successfully\n");
 
-    if (environmental_sensor_read(&data) != 0)
-    {
-        printf("Failed to read environmental data\n");
-        return;
-    }
-
-    printf("Temperature: %.1f C\n", data.temperature);
-    printf("Humidity: %.1f %%\n", data.humidity);
+    xTaskCreate(
+        sensor_monitor_task,
+        "sensor_monitor",
+        4096,
+        NULL,
+        5,
+        NULL
+    );
 }
