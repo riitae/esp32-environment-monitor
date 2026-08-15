52e6710643c1b5bc0f1bc4510c1c14d49e2c7b80
riitae <rita.emiri3@student.uni-pr.edu>
2026-08-15 10:25:44 +0200

    Integrate environmental sensor into application

--- main/esp32-environment-monitor.c
+++ main/esp32-environment-monitor.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
+#include "environmental_sensor.h"
 
 void app_main(void)
 {
+    environmental_data_t data;
 
+    if (environmental_sensor_init() != 0)
+    {
+        printf("Sensor initialization failed\n");
+        return;
+    }
+
+    printf("Environmental sensor initialized successfully\n");
+
+    if (environmental_sensor_read(&data) != 0)
+    {
+        printf("Failed to read environmental data\n");
+        return;
+    }
+
+    printf("Temperature: %.1f C\n", data.temperature);
+    printf("Humidity: %.1f %%\n", data.humidity);
 }
